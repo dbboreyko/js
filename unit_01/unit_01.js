9dfa089adb8d52934db5a5a88674f29a5de58033
dbboreyko <d.boreyko@ortlyc.kyiv.ua>
2026-05-04 13:09:02 +0300

    v1

---
+++ unit_01/unit_01.js
@@ -0,0 +1,107 @@
+// Task 1.
+console.log('Даня Борейко');
+
+// Task 2.
+
+
+console.log('3');
+
+
+
+// Task 3.
+
+console.log('Ласкаво '+'запрошуємо '+'на навчання');
+
+
+// Task 4.
+
+alert('2019');
+
+
+// Task 5.
+
+console.log(2023 - 200);
+
+// Task 6.
+
+document.getElementById('six').textContent = 'Hello World';
+
+// Task 7.
+
+document.getElementById('seven').textContent = 12 * 12;
+
+// Task 8.
+
+let g = document.querySelector('.out-8');
+g.innerHTML = 'task-8';
+
+// Task 9.
+
+let d = document.querySelector('.out-9');
+d.innerHTML = 'world';
+
+
+// Task 10.
+let c = document.querySelector('.out-10');
+c.innerHTML = '<h2>Hi</h2>';
+
+// Task 11.
+
+
+let b = document.querySelector('.out-11');
+b.innerHTML = '123';
+b.innerHTML += '456';
+
+
+// Task 12.
+
+let a = document.querySelector('.out-12');
+a.innerHTML = 3.1415;
+
+// Task 13.
+
+let out13 = document.querySelector('.out-13');
+out13.innerHTML = '<img src="product.png" alt="js">';
+
+// Task 14.
+
+let z1 = 6;
+let z2 = 3;
+let out14 = document.querySelector('.out-14');
+out14.innerHTML = z1 * z2;
+// Task 15.
+
+let y1 = 6;
+let y2 = 3;
+let out15 = document.querySelector('.out-15');
+out15.innerHTML = y1 / y2;
+
+// Task 16.
+
+let x1 = 'Hello';
+let x2 = 5;
+let out16 = document.querySelector('.out-16');
+out16.innerHTML = x1 + x2;
+
+
+// Task 17.
+let out17 = document.querySelector('.out-17');
+console.log(out17);
+
+// Task 18.
+
+let out18 = document.querySelector('.out-18');
+console.log(out18);
+out18 = 5;
+console.log(out18);
+
+// Task 19.
+
+let out19 = document.querySelector('.out-19');
+console.log(out19);
+let div19 = document.querySelector('.out-19-test')
+console.log(div19);
+
+// Task 20
+let out20 = document.querySelector('.out-20');
+out20.textContent = '<h2>Hi</h2>'
